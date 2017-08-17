2481ca23030a649f592487b0572c12437072bf35
ricardoluizkoch <wow1700@gmail.com>
2017-08-17 17:22:24 -0300

    Desenvolvimento da classe Revista com atributo

---
+++ src/model/Revista.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Ricardo Luiz Koch
+ *
+ */
+public class Revista {
+    private int codigo, quantidadePaginas;
+    private String nome, descricao;
+}
